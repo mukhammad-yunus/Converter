62a16454fe6b20c5f7fb03b4c8dee1881ac81f9d
mukhammad-yunus <dr.mukhammadyunus@gmail.com>
2023-02-15 17:28:41 +0500

    updates in css and html files
    deleted convert button, changed some bck colors and text colors

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const input = document.getElementById('input-el')
-let convertBtn = document.getElementById("convert-btn")
 let lengthEL = document.getElementById("length")
 let volumeEL = document.getElementById("volume")
 let massEL = document.getElementById("mass")
 
 input.addEventListener('input', function() {
   this.style.width = ((this.value.length + 1) * 40) + 'px';
-});
-
-convertBtn.addEventListener("click", function(){
-    let inputNum = Number(input.value)
-    lengthEL.textContent = `${inputNum} m = ${(inputNum*3.28084).toFixed(3)} ft | ${inputNum} ft = ${(inputNum*0.3048).toFixed(3)} m`
-    volumeEL.textContent = `${inputNum} l = ${(inputNum*0.264172).toFixed(3)} gal | ${inputNum} gal = ${(inputNum*3.78541).toFixed(3)} l`
-    massEL.textContent = `${inputNum} kg = ${(inputNum*2.20462).toFixed(3)} lb | ${inputNum} lb = ${(inputNum*0.453592).toFixed(3)} kg`
+  
+  let inputNum = Number(input.value)
+  lengthEL.textContent = `${inputNum} m = ${(inputNum*3.28084).toFixed(3)} ft | ${inputNum} ft = ${(inputNum*0.3048).toFixed(3)} m`
+  volumeEL.textContent = `${inputNum} l = ${(inputNum*0.264172).toFixed(3)} gal | ${inputNum} gal = ${(inputNum*3.78541).toFixed(3)} l`
+  massEL.textContent = `${inputNum} kg = ${(inputNum*2.20462).toFixed(3)} lb | ${inputNum} lb = ${(inputNum*0.453592).toFixed(3)} kg`
 })
 
 
